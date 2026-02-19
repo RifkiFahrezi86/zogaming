9373325ffc75d8fa981c3bc35c5d6e5f32d13c12
Rifki Fahrezi <rifkifka@email.com>
2026-02-19 21:10:13 +0800

    pri

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -47,7 +47,7 @@ export default function ShopPage() {
                             Home
                         </Link>
                         <span>&gt;</span>
-                        <span className="text-white">Our Shops</span>
+                        <span className="text-white">Our Shop</span>
                     </nav>
                 </div>
             </section>
